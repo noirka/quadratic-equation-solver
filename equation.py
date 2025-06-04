cf3dfdc9c28bb900b9cd0e56fc067eb7a57e26bd
noirka <nuarveronika@gmail.com>
2025-06-04 09:37:05 +0300

    Add output formatting functions

--- equation.py
+++ equation.py
@@ -7,6 +7,7 @@ def solve_quadratic(a, b, c):
     if a == 0:
         raise ValueError("a cannot be 0")
     
+    # Обчислюємо дискримінант
     discriminant = b * b - 4 * a * c
     
     if discriminant < 0:
@@ -19,3 +20,21 @@ def solve_quadratic(a, b, c):
         root1 = (-b + sqrt_discriminant) / (2 * a)
         root2 = (-b - sqrt_discriminant) / (2 * a)
         return (2, [root1, root2])
+
+
+def format_equation(a, b, c):
+    return f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0"
+
+
+def print_results(a, b, c, num_roots, roots):
+    print(format_equation(a, b, c))
+    
+    if num_roots == 0:
+        print("There are 0 roots")
+    elif num_roots == 1:
+        print("There are 1 roots")
+        print(f"x1 = {roots[0]}")
+    else:
+        print("There are 2 roots")
+        print(f"x1 = {roots[0]}")
+        print(f"x2 = {roots[1]}")
